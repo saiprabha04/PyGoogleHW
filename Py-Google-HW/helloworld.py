28b648aba9d14a33f76f6c9872db6fb4cbb3c907
saiprabha04 <54578968+saiprabha04@users.noreply.github.com>
2019-09-28 15:54:56 +0530

    Update helloworld.py

--- Py-Google-HW/helloworld.py
+++ Py-Google-HW/helloworld.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     print('Hello World - welcome to NIIT University')
-    print("done editing")
-    print("done pulling")
+    print('done editing')
+    print('done pulling')
